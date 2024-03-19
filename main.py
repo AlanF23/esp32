abeefcbe4d8687d7c8b8941c8a2ae2aee1b85458
Alan <alanf200023unam@gmail.com>
2024-03-19 17:01:57 -0300

    medicion de temp y humedad cada 2s

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import dht, machine
+import time
 
-d = dht.DHT22(machine.Pin(13))
-d.measure()
-d.temperature()
-d.humidity()
+d = dht.DHT22(machine.Pin(25))
+
+while True:
+    d.measure()
+    d.temperature()
+    d.humidity()
+    print("La temperatura es: ",d.temperature())
+    print("La humedad es: ",d.humidity())
+    time.sleep(2)
