314ccdc672f95e7e69e24cce232a881c58ecd892
Alan <alanf200023unam@gmail.com>
2024-03-19 15:49:02 -0300

    bandera agregada

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ print("esperando pulsador")
 
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
+contador = 0
+flag=True
 
 while True:
-    if sw.value():
+    if sw.value() and flag:
+        flag = False
         led.value(not led.value())
+        contador =+ 1
+        print(contador)
+    if not sw.value():
+        flag = True
     time.sleep_ms(5)
