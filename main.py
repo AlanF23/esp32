4cf3713c48873d2de99fe94df85722ad356264ad
AlanF23 <55410964+AlanF23@users.noreply.github.com>
2025-08-14 18:43:30 -0300

    agregado de actuadores, falta definir pines y agregar funcionamiento del alimentador

--- main.py
+++ main.py
@@ -30,9 +30,68 @@ adc = machine.ADC(machine.Pin(34))
 adc.atten(machine.ADC.ATTN_11DB)  # Rango 0–3.6V
 adc.width(machine.ADC.WIDTH_12BIT)  # Resolución 0–4095
 
+calentador = "apagado"
+flagcalentador = 1
+accioncalentador = machine.Pin(13, machine.Pin.OUT)
+
+ventilador = "apagado"
+flagventilador = 1
+accionventilador = machine.Pin(12, machine.Pin.OUT)
+
+filtro = "apagado"
+flagfiltro = 1
+accionfiltro = machine.Pin(11, machine.Pin.OUT)
+
+alimentar = 0
+alimentador = machine.Pin(2, machine.Pin.OUT)
+alimentador.value(0)
+
+datos = {
+    'temperatura': 0.0,
+    'turbidez': 0.0,
+    'temperaturasuperior': 25.0,
+    'setpointturbidez': 2800.0,
+    'periodo': 5,
+    'modo': "manual"
+    }
 
 def sub_cb(topic, msg, retained):
-    print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
+    global alimentar
+    global calentador, flagcalentador
+    global ventilador, flagventilador
+    global filtro, flagfiltro
+    topico = topic.decode()
+    mensaje = msg.decode()
+    print('Topic = {} -> Valor = {}'.format(topico, mensaje))
+    if topico == 'setpointtemperatura':
+        datos['setpointtemperatura']=float(mensaje)
+
+    elif topico == 'setpointturbidez':
+        datos['setpointturbidez']=float(mensaje)
+
+    elif topico == 'periodo':
+        datos['periodo']=int(mensaje)
+
+    elif topico == 'modo':
+        datos['modo']=mensaje.lower()
+        if datos['modo'] == 'manual':
+            flagventilador = 1
+            flagcalentador = 1
+            flagfiltro = 1
+        if datos['modo'] == 'auto':
+            flagventilador = 0
+            flagcalentador = 0
+            flagfiltro = 0
+    
+    elif topico == 'ventilador':
+        ventilador = mensaje.lower()
+    elif topico == 'calentador':
+        calentador = mensaje.lower()
+    elif topico == 'filtro':
+        filtro = mensaje.lower()
+
+    elif topico == 'alimentar':
+        alimentar = int(mensaje)
 
 async def wifi_han(state):
     print('Wifi is ', 'up' if state else 'down')
@@ -40,7 +99,15 @@ async def wifi_han(state):
 
 # If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
 async def conn_han(client):
-    await client.subscribe(config['client_id'], 1)
+    await client.subscribe('prueba/'+CLIENT_ID, 1)
+    await client.subscribe('setpointtemperatura', 1)
+    await client.subscribe('setpointturbidez', 1)
+    await client.subscribe('periodo', 1)
+    await client.subscribe('modo', 1)
+    await client.subscribe('ventilador', 1)
+    await client.subscribe('calentador', 1)
+    await client.subscribe('filtro', 1)
+    await client.subscribe('alimentar', 1)
 
 async def main(client):
     await client.connect()
@@ -58,8 +125,7 @@ async def main(client):
             voltaje_real = voltaje * 1.5  # Ajuste del voltaje
             ntu = -1120.4 * voltaje_real**2 + 5742.3 * voltaje_real - 4352.9
             ntu = max(0, round(ntu, 2))  # Limitar a 0 si da negativo, redondear
-            print("Voltaje: ", voltaje)
-            #await client.publish('prueba/turbidez', str(ntu), qos=1)
+            #print("Voltaje: ", voltaje)
             datos=json.dumps(OrderedDict([
                 ('temperatura',temp),
                 ('turbidez',ntu)
@@ -67,7 +133,59 @@ async def main(client):
             await client.publish('prueba/'+CLIENT_ID, datos, qos = 1)
         except Exception as e:
             print("Error al leer los sensores:", e)
-        await asyncio.sleep(5)  # Broker is slow
+        try:
+            if flagventilador == 1:
+                if ventilador == 'encendido':
+                    accionventilador.value(0)
+                    print("Ventilador encendido manualmente")
+                elif ventilador == 'apagado':
+                    accionventilador.value(1)
+                    print("Ventilador apagado manualmente")
+            else:
+                if datos['temperatura'] > (datos['setpointtemperatura']+1.0):
+                    accionventilador.value(0)
+                else:
+                    accionventilador.value(1)
+        except OSError as e:
+            print("Ventilador NO Funciona")
+        try:
+            if flagcalentador == 1:
+                if calentador == 'encendido':
+                    accioncalentador.value(0)
+                    print("Calentador encendido manualmente")
+                elif calentador == 'apagado':
+                    accioncalentador.value(1)
+                    print("Calentador apagado manualmente")
+            else:
+                if datos['temperatura'] < (datos['setpointtemperatura']-1.0):
+                    accioncalentador.value(0)
+                else:
+                    accioncalentador.value(1)
+        except OSError as e:
+            print("Calentador NO Funciona")
+        try:
+            if flagfiltro == 1:
+                if filtro == 'encendido':
+                    accionfiltro.value(0)
+                    print("Filtro encendido manualmente")
+                elif filtro == 'apagado':
+                    accionfiltro.value(1)
+                    print("Filtro apagado manualmente")
+            else:
+                if datos['turbidez'] > datos['setpointturbidez']:
+                    accionfiltro.value(0)
+                else:
+                    accionfiltro.value(1)
+        except OSError as e:
+            print("Filtro NO Funciona")
+        try:
+            if alimentar == 1:
+                alimentador.value(1)
+                print("Alimentando peces")  #Aca tengo que agregar lo del alimentador
+                alimentar = 0
+        except OSError as e:
+            print("Alimentador NO Funciona")
+        await asyncio.sleep(datos['periodo'])  # Broker is slow
 
 # Define configuration
 config['subs_cb'] = sub_cb
